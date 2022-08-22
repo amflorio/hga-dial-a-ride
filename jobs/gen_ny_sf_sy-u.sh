3198455cf42b75fd02b6a44f0dd7c44da52599ae
Alexandre Florio <aflorio@gmail.com>
2022-08-22 05:39:12 +0200

    Invert order for more efficient allocation

--- jobs/gen_ny_sf_sy-u.sh
+++ jobs/gen_ny_sf_sy-u.sh
@@ -9,20 +9,20 @@ fi
 
 echo '#!/bin/bash' > $JOBS_FILE
 echo '#SBATCH -N 1' >> $JOBS_FILE
-echo '#SBATCH -t 48:00:00' >> $JOBS_FILE
+echo '#SBATCH -t 24:00:00' >> $JOBS_FILE
 echo '#SBATCH --mem=90G' >> $JOBS_FILE
 echo '#SBATCH --mail-type=BEGIN,END' >> $JOBS_FILE
 echo '#SBATCH --mail-user=a.de.macedo.florio@tue.nl' >> $JOBS_FILE
 echo 'RUNPATH=$HOME/hga-dial-a-ride/src' >> $JOBS_FILE
 echo 'cd $RUNPATH' >> $JOBS_FILE
 
-INSTANCES="ny sf sy-u"
 REQS="2000 4000 6000 8000 10000"
+INSTANCES="ny sf sy-u"
 DRIVERS="30 60 90 120 150"
 CAPACITIES="2 4 8 16 32 64"
 
-for inst in $INSTANCES; do
-    for req in $REQS; do
+for req in $REQS; do
+    for inst in $INSTANCES; do
         for drvs in $DRIVERS; do
             for cap in $CAPACITIES; do
                 echo "\$RUNPATH/main $inst $req $drvs $cap > \$RUNPATH/../out/$inst-$req-$drvs-$cap.out &" >> $JOBS_FILE
