4276b98b95004fd0ab6c3232aef9bff376133da7
Alexandre Florio <aflorio@gmail.com>
2022-08-27 15:39:22 -0700

    Implement a more memory-conscious cost matrix

--- src/Graph.h
+++ src/Graph.h
@@ -9,21 +9,28 @@
 class Graph {
     private:
         size_t N;                               // number of nodes
-        std::vector<std::vector<double>> costs; // upper-triangular cost matrix
+        std::vector<std::vector<float>> csts;   // triangular cost matrix
         std::vector<std::pair<size_t, size_t>> bucketMatching() const;
         Tree connectTrees(const std::vector<Tree>& trees) const;
         Tree mergeCluster(const std::unordered_set<size_t>& active, size_t k)
                 const;
-    public:
-        Graph(size_t N) : N{N}, costs(N, std::vector<double>(N, 0)) {}
-        double edgeCost(size_t i, size_t j) const
-                {return i<j?costs[i][j]:costs[j][i];}
-        std::vector<std::pair<size_t, size_t>> minCostMatching();
-        double MSTCost() const;
-        Tree MST() const;
-        void setEdgeCost(size_t i, size_t j, double c) {
+        float cost(size_t i, size_t j) const {
             assert(i<j);
-            costs.at(i).at(j)=c;
+            return csts.at(i).at(j-i-1);
+        }
+    public:
+        Graph(size_t N) : N{N} {
+            for (size_t i=0; i<N; ++i)
+                csts.push_back(std::vector<float>(N-i-1, 0));
+        }
+        float edgeCost(size_t i, size_t j) const
+                {return i<j?cost(i, j):cost(j, i);}
+        std::vector<std::pair<size_t, size_t>> minCostMatching();
+        float MSTCost() const;
+        Tree MST() const;
+        void setEdgeCost(size_t i, size_t j, float c) {
+            assert(i<j);
+            csts.at(i).at(j-i-1)=c;
         }
         Tree unrootedKMST(const size_t k) const;
 };
